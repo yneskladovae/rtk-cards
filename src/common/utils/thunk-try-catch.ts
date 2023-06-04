46a3e78aeab9773e5efcf1cff436e91d45a21b0f
yneskladovae <yneskladovae27@gmail.com>
2023-06-05 00:26:53 +0300

    refactor: refactor thunk-try-catch func

--- src/common/utils/thunk-try-catch.ts
+++ src/common/utils/thunk-try-catch.ts
@@ -1,6 +1,8 @@
 import { BaseThunkAPI } from "@reduxjs/toolkit/dist/createAsyncThunk";
 import { AppDispatch, RootState } from "app/store";
 import { toast } from "react-toastify";
+import { AxiosError, isAxiosError } from "axios";
+import { appActions } from "features/app/app.slice";
 
 export const thunkTryCatch = async (
   thunkAPI: BaseThunkAPI<RootState, any, AppDispatch, unknown>,
@@ -9,9 +11,16 @@ export const thunkTryCatch = async (
   const { dispatch, rejectWithValue } = thunkAPI;
   try {
     return await promise();
-  } catch (e: any) {
-    const error = e.response ? e.response.data.error : e.message;
-    toast.error(error);
+  } catch (e) {
+    let errorMessage = "";
+    if (isAxiosError(e)) {
+      errorMessage = e?.response?.data()?.error;
+    } else if (e instanceof Error) {
+      errorMessage = `Native error ${e.message}`;
+    } else {
+      errorMessage = JSON.stringify(e);
+    }
+    toast.error(errorMessage);
     return rejectWithValue(null);
   }
 };
